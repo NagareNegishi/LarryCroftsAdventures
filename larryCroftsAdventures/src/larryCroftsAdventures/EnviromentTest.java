346de945868c9c8b871c0dff428befaa5408943e
Joshua Neylan <JoshuaNeylan@gmail.com>
2024-09-12 20:29:36 +1200

    Creating base project with enviroment test class #7

---
+++ larryCroftsAdventures/src/larryCroftsAdventures/EnviromentTest.java
@@ -0,0 +1,7 @@
+package larryCroftsAdventures;
+
+public class EnviromentTest {
+	public static void main(String[] args) {
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+    }
+}
