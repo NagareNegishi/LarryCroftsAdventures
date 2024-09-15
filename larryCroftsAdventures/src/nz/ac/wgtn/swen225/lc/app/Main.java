05da65747f72b1cee352c89a56905419c833a8eb
negimakushi <132905416+negimakushi@users.noreply.github.com>
2024-09-15 13:46:18 +1200

    test #2

--- larryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/app/Main.java
+++ larryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/app/Main.java
@@ -3,5 +3,6 @@ package nz.ac.wgtn.swen225.lc.app;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Java Version: " + System.getProperty("java.version"));
+		
 	}
 }
