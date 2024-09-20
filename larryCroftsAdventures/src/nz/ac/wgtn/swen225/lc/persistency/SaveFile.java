65aeea506cdd9c67132c80a5de33b869fdef3e16
AdamT-code <adam.james.tither@gmail.com>
2024-09-20 15:57:29 +1200

    Cleaned up superflous methods

--- larryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/persistency/SaveFile.java
+++ larryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/persistency/SaveFile.java
@@ -36,56 +36,6 @@ public class SaveFile implements Saver{
     }
     
     
-    /**
-     * Test for objectmapper
-     * @param fileName
-     * @param info
-     * @return
-     */
-    public static boolean saveString(String fileName, String info) {
-    	
-    	assert info != null && !info.isEmpty();
-    	assert fileName != null && !fileName.isEmpty();
-    	
-    	ObjectMapper mapper = new ObjectMapper();
-    	try{
-            mapper.writeValue( new FileOutputStream(fileName), info);
-            return true;
-        } catch(IOException e){
-            e.printStackTrace();
-        }
-        // Failed to send
-        return false;
-    	
-    }
-    
-    
-    
-    
-    
-    /**
-     * Generic save for all objects. DOES NOT WORK
-     * @param <T>
-     * @param fileName
-     * @param obj
-     * @return
-     */
-    public static boolean saveCanary(String fileName, Canary obj) {
-    	
-    	assert obj != null;
-    	
-    	ObjectMapper mapper = new ObjectMapper();
-    	try{
-            mapper.writeValue( new FileOutputStream(fileName), obj);
-            return true;
-        } catch(IOException e){
-            e.printStackTrace();
-        }
-        // Failed to send
-        return false;
-    	
-    }
-    
     
     /**
      * Generic method to save JSON of any object
@@ -96,6 +46,7 @@ public class SaveFile implements Saver{
      */
     public static <T> boolean saveObj(String fileName, T obj) {
     	assert obj != null;
+    	assert !fileName.isEmpty();
     	
     	ObjectMapper mapper = new ObjectMapper();
     	
--- larryCroftsAdventures/src/test/nz/ac/wgtn/swen225/lc/persistency/PersistencyTest.java
+++ larryCroftsAdventures/src/test/nz/ac/wgtn/swen225/lc/persistency/PersistencyTest.java
@@ -22,41 +22,35 @@ public class PersistencyTest {
 //        assert SaveFile.saveGame("Test1", gm);
 //    }
 
+	/**
+	 * Test for Juint
+	 */
     @Test
     public void test() {
         Integer i = 0;
         assert i != null;
     }
     
-    @Test
-    public void testSave() {
-    	String info = "Hello did this work?";
-    	assert SaveFile.saveString("testSave", info);
-    }
-    
     
     /**
-     * Check how Canary serialises into JSON
+     * Test for ObjectMaper
      */
-    @Test public void canaryTest() {
-    	Canary c = new Canary("Lary", 500);
-    	assert SaveFile.saveCanary("BestLarry", c);
-    }
-    
     @Test public void objTest() {
     	Canary c = new Canary("ObjBird", 1000);
     	assert SaveFile.saveObj("ObjectTest", c);
-    	
     }
     
+    // TODO
     @Test public void GameStateTest() {
     	Maze maze = Maze.createBasicMaze(5, 5);
 		Chap chap = new Chap(2,2);
 		GameStateInterface test = new GameState(maze,chap,1);
 		//assert SaveFile.save
-    	
     }
     
+    /**
+     * Test serialisation of GameState
+     */
     @Test public void GameControlTest() {
     	GameStateControllerInterface gs = new GameStateController(5, 5, 2, 2, 1);
     	assert SaveFile.saveGame("GameSaveTest", gs);
