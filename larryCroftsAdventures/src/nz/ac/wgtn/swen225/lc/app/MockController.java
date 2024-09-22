d1756326f7f5590efefb359ee6092ce9cd3bd3a9
negimakushi <132905416+negimakushi@users.noreply.github.com>
2024-09-22 20:52:34 +1200

    updated MockController/ showcase Null Object Pattern #2

--- larryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/app/MockController.java
+++ larryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/app/MockController.java
@@ -1,5 +1,8 @@
 package nz.ac.wgtn.swen225.lc.app;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import nz.ac.wgtn.swen225.lc.domain.Chap;
 import nz.ac.wgtn.swen225.lc.domain.Maze;
 
@@ -7,7 +10,7 @@ public class MockController extends Controller{
 	public Chap chap;
 	public Maze maze;
 	public MockController(Chap chap, Maze maze) {
-		super(chap,maze,null);
+		super(chap,maze,createMockActionBindings());
 		this.chap = chap;
 		this.maze = maze;
 	}
@@ -16,6 +19,22 @@ public class MockController extends Controller{
 	}
 	public Maze getMaze() {
 		return maze;
+	}
+
+	/**
+	 * mocking the action bindings
+	 * with Null Object Pattern
+	 */
+	private static Map<String, Runnable> createMockActionBindings() {
+		Map<String, Runnable> mockBindings = new HashMap<>();
+        mockBindings.put("exitWithoutSaving", () -> {});
+        mockBindings.put("exitAndSave", () -> {});
+        mockBindings.put("resumeSavedGame", () -> {});
+        mockBindings.put("startNewGame1", () -> {});
+        mockBindings.put("startNewGame2", () -> {});
+        mockBindings.put("pause", () -> {});
+        mockBindings.put("unpause", () -> {});
+        return mockBindings;
 	}
 
 }
