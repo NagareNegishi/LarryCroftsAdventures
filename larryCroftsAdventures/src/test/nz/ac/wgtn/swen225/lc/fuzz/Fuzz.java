95ec202d6620e758c7a6a47f243935362fb3b3ee
negimakushi <132905416+negimakushi@users.noreply.github.com>
2024-09-22 20:25:36 +1200

    deleted line "import nz.ac.wgtn.swen225.lc.app.App;"
    from Fuzz.java.
    since App is private and it wont compile #6

--- larryCroftsAdventures/src/test/nz/ac/wgtn/swen225/lc/fuzz/Fuzz.java
+++ larryCroftsAdventures/src/test/nz/ac/wgtn/swen225/lc/fuzz/Fuzz.java
@@ -2,17 +2,11 @@ package test.nz.ac.wgtn.swen225.lc.fuzz;
 
 import java.util.Random;
 
-import javax.swing.SwingUtilities;
-
 import org.junit.Test;
 
-import nz.ac.wgtn.swen225.lc.app.App;
 import nz.ac.wgtn.swen225.lc.app.MockController;
 import nz.ac.wgtn.swen225.lc.domain.Chap;
 import nz.ac.wgtn.swen225.lc.domain.Maze;
-
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 
 public class Fuzz {
