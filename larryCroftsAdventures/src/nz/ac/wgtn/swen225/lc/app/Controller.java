2367ca304acc7a18cc17f156c04beb89b64e1934
negishnaga <negishnaga@ecs.vuw.ac.nz>
2024-09-23 20:38:39 +1200

    documentation Keys #2

--- larryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/app/Controller.java
+++ larryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/app/Controller.java
@@ -10,10 +10,6 @@ import nz.ac.wgtn.swen225.lc.domain.Maze;
 
 /**
  * Controller class that extends Keys class
- * 
- * will be public after the merge, but need to be private before submission
- * both class and constructor
- * 
  */
 class Controller extends Keys{
 /**
