011fe35e19012d8ff5da26a9e4c827f14778e3b4
Kourie <80491969+Kourie@users.noreply.github.com>
2024-10-13 19:35:30 +1300

    ensuring everything's commited right#3

--- larryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
+++ larryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
@@ -3,35 +3,7 @@ package nz.ac.wgtn.swen225.lc.renderer;
 
 import java.awt.Color;
 
-/**
- * This is the class that handles all the rendering logic, 
- * 
- * @author Marwan Mohamed
- * @studentID 300653693
- * 
- */
-
-
-/**to get things started, I should have a way to identify the names or the images i'm going to load
- * I should make a canvas thing to start, on launch
- * then, i can have an update method.
- * 
- * there are a few ways to make this efficient, generally speaking, using enums for singlton is the best
- * there should be a loop, to draw everything around the player, with them being centered
- * for now, I can hardcode values in, but it's much better to use the img's uniform width of sorts
- * 
- * I should assume that Jpanel will run things well on the GPU for now.
- * for now, let's get a jPanel going with a canvas to draw on, then an image
- * after which. let's see the image move whenever there's some sort of update happening
- * 
- */
-
-
-
-import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Point;
 import java.util.List;
 
 import nz.ac.wgtn.swen225.lc.domain.*;
